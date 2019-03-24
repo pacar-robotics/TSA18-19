5e57b8add391bf7273b4864b81f562cf2fa978d8
rsquared226 <rahulram226@gmail.com>
2019-03-24 15:43:41 -0400

    Create driveWheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -5,8 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class MainTeleOp extends LinearOpMode {
+    Robot robot;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
 
+        while (opModeIsActive()) {
+            driveWheels();
+        }
+    }
+
+    private void initialize() {
+        robot = new Robot(hardwareMap);
+    }
+
+    private void driveWheels() {
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        robot.wheelTopLeft.setPower(v1);
+        robot.wheelTopRight.setPower(v2);
+        robot.wheelBotLeft.setPower(v3);
+        robot.wheelBotRight.setPower(v4);
     }
 }
