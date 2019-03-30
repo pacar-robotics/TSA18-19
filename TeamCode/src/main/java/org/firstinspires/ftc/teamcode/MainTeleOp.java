dd0c37b69fee0b2aa712ffa460c25e294eda8913
rsquared226 <rahulram226@gmail.com>
2019-03-30 13:00:24 -0400

    Supressed Unused Warning Using Annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@SuppressWarnings("unused")
 @TeleOp
 public class MainTeleOp extends LinearOpMode {
     private Robot robot;
